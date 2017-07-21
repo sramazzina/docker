b621e85b50bb9542b96d8c930dd24cb95aaad62c
Sergio Ramazzina <sergio.ramazzina@serasoft.it>
2017-07-21 15:01:20 +0200

    Latest refinement to sos berlin jobscheduler container's image

--- jobscheduler/other/wait-for-postgres.sh
+++ jobscheduler/other/wait-for-postgres.sh
@@ -4,11 +4,10 @@
 set -e
 
 host="$1"
-port="$2"
 shift
 cmd="$@"
 
-until psql -h "$host" -p "$port" -U "postgres" -c '\l'; do
+until psql -h "$host" -U "postgres" -c '\l'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
