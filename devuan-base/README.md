784e248d5b1175a1990c467acf6ef80852f9722e
Sergio Ramazzina <sergio.ramazzina@serasoft.it>
2021-10-01 14:39:13 +0200

    Project refactoring. Removed unused projects

--- devuan-base/README.md
+++
@@ -1,1 +0,0 @@
-# devuan-base
--- jobscheduler/other/pg_setup.sql
+++
@@ -1,9 +0,0 @@
-CREATE USER @@DB_USER@@ WITH SUPERUSER PASSWORD '@@DB_USER_PWD@@';
-CREATE DATABASE @@DB_SCHEMA@@ OWNER @@DB_USER@@ ENCODING 'UNICODE' TEMPLATE template1;
--- Needed to support PostgreSQL Rel > 9.1
-alter user @@DB_USER@@ set standard_conforming_strings = on;
-alter user @@DB_USER@@ set bytea_output = 'escape';
--- UPDATE pg_database SET datistemplate = FALSE WHERE datname = 'template1';
--- DROP DATABASE template1;
--- CREATE DATABASE template1 WITH TEMPLATE = template0 ENCODING = 'UNICODE';
--- UPDATE pg_database SET datistemplate = TRUE WHERE datname = 'template1';
